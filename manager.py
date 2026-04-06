033545cd77b5ff8cd25ac760d1afeaf505ca4821
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2026-04-05 21:15:36 -0300

    feat: add function to add tasks

--- manager.py
+++ manager.py
@@ -1,3 +1,11 @@
+def add_task(tasks, task_name):
+    task = {"task": task_name, "closed": False}
+    tasks.append(task)
+    print(f"Tarefa '{task_name}' adicionada com sucesso!")
+    return
+
+tasks = []
+
 while True:
     print("\nMenu do Gerenciador de Lista de tarefas:")
     print("1. Adicionar tarefa")
@@ -9,7 +17,10 @@ while True:
 
     choice = input("Digite a sua escolha: ")
 
-    if choice == "6":
+    if choice == "1":
+        task_name = input("\nDigite o nome da tarefa: ")
+        add_task(tasks, task_name)
+    elif choice == "6":
         break
 
 print("Programa Finalizado!")
