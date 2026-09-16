1ec95c66ecf1daa21a6612bd63c267030249126e
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2026-09-16 20:58:25 -0300

    feat: adds manager menu

---
+++ manager.py
@@ -0,0 +1,15 @@
+while True:
+    print("----- Task manager menu -----")
+    print("1. Add task")
+    print("2. View tasks")
+    print("3. Update task")
+    print("4. Closed task")
+    print("5. Remove closed tasks")
+    print("6. Exit")
+
+    choice = input("\nType your choice: ")
+
+    if choice == "6":
+        break
+
+print("Finish program!")
