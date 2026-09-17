99c2b9719a0a5ea02367f3e282a10b5b39ad2488
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2026-09-17 16:38:02 -0300

    feat: create function to add task

--- manager.py
+++ manager.py
@@ -1,5 +1,12 @@
+def add_task(tasks, task_name):
+    task = {"name": task_name, "closed": False}
+    tasks.append(task)
+    print(f"\nTask '{task_name}' was added successfully!")
+
+tasks = []
+
 while True:
-    print("----- Task manager menu -----")
+    print("\n----- Task manager menu -----")
     print("1. Add task")
     print("2. View tasks")
     print("3. Update task")
@@ -9,7 +16,10 @@ while True:
 
     choice = input("\nType your choice: ")
 
-    if choice == "6":
+    if choice == "1":
+        task_name = input("Enter the name of the task: ")
+        add_task(tasks, task_name)
+    elif choice == "6":
         break
 
 print("Finish program!")
